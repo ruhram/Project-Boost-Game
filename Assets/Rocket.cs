1faad45eac137b8a0336d7257716220a1cabf0ab
LAPTOP-EAT55AGK\Oscarium <rullyrgandadiwirya@gmail.com>
2021-02-17 22:32:37 +0700

    Tagging

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -21,7 +21,24 @@ public class Rocket : MonoBehaviour
         Thrust();
         Rotate();
     }
-
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                {
+                    print("ok");
+                }break;
+            case "Fuel":
+                {
+                    print("Fuel");
+                }break;
+            default:
+                {
+                    print("Dead");
+                }break;
+        }
+    }
     void Rotate()
     {
         float RotationThisFrame = RcsThrust * Time.deltaTime;
